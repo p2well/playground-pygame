d8df835b9fe7f4e7cbbf0b2377e92527f02e8095
p2well <p2well@outlook.com>
2025-09-28 05:56:02 +0200

    Display the score

--- game-runner.py
+++ game-runner.py
@@ -1,18 +1,24 @@
 import pygame
 from sys import exit
 
+
+def display_score():
+    current_time = pygame.time.get_ticks() - start_time
+    score_surf = test_font.render(f'Score: {current_time // 1000}', False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center=(400, 50))
+    screen.blit(score_surf, score_rect)
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('fonts/pixeltype.ttf', 50)
 game_active = True
+start_time = 0
 
 sky_surf = pygame.image.load('graphics/sky.png').convert()
 ground_surf = pygame.image.load('graphics/ground.png').convert()
-
-score_surf = test_font.render('My game', False, (64, 64, 64))
-score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright=(800, 300))
@@ -37,13 +43,13 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = pygame.time.get_ticks()
 
     if game_active:
         screen.blit(sky_surf, (0, 0))
         screen.blit(ground_surf, (0, 300))
 
-        pygame.draw.rect(screen, "#c0e8ec", score_rect, 0, 10)
-        screen.blit(score_surf, score_rect)
+        display_score()
 
         snail_rect.x -= 4
         if snail_rect.right <= 0:
